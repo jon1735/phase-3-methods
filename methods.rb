04ba4bd4b3f09ba2a02af5a6e5c763b28aa5f81f
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-08-04 15:11:36 -0400

    commit tree 0eafd59d415510a83a126427076e202052c5a95d

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
-# Your code here!
+# 1
+def greet_programmer()
+  puts "Hello, programmer!"
+end
+
+# 2
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+# 3
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+# 4
+def add(num1, num2)
+  puts num1 + num2
+  num1 = (8)
+  num2 = (7)
+end
+
+#5
+def halve(num)
+  return nil unless num.class == Integer 
+  num = (6/2)
+end
